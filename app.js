1fc86f3a1d7eaac5d88603183cd6d5fd5202c1e1
oscar <oscar@o2webdev.com>
2018-10-03 20:12:53 -0400

    clean up

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// var restify = require('restify');
-// var builder = require('botbuilder');
-
-// // Setup Restify Server
-// var server = restify.createServer();
-// server.listen(process.env.port || process.env.PORT || 3978, function () {
-//     console.log('%s listening to %s', server.name, server.url);
-// });
-
-// // Create chat connector for communicating with the Bot Framework Service
-// var connector = new builder.ChatConnector({
-//     appId: process.env.MicrosoftAppId,
-//     appPassword: process.env.MicrosoftAppPassword
-// });
-
-// // Listen for messages from users 
-// server.post('/api/messages', connector.listen());
-
-// // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-// var bot = new builder.UniversalBot(connector, function (session) {
-//     // session.send("You said: %s", session.message.text);
-//     builder.Prompts.text(session, 'Hello! What is your name?');
-// },
-// );
-
 // This loads the environment variables from the .env file
 require('dotenv-extended').load();
 
